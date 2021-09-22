9cd53ae12aad0a8167b9a58b96c16b89747b9ef9
Parinith Shekar <parinith.1114@gmail.com>
2021-09-22 09:41:54 +0200

    Initial commit

---
+++ src/main/java/dev/owow/wizkidmanager2000/WizkidManager2000Application.java
@@ -0,0 +1,13 @@
+package dev.owow.wizkidmanager2000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizkidManager2000Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WizkidManager2000Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/owow/wizkidmanager2000/WizkidManager2000ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.owow.wizkidmanager2000;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizkidManager2000ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
